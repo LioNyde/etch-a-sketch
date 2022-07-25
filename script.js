366abd2b662432597aab6fe205ee76e64645161e
lioNyde <nanitf7@gmail.com>
2022-07-25 16:18:18 +0800

    Background Color Picker

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //paint
 
 let color = 'black';
+let bgcolor = 'white'
 
 const BOARD = document.querySelector('#grid')
 
@@ -14,13 +15,14 @@ function fillBoard(size){
    for (let i = 0; i < totalsize; i++) {
       const BOX = document.createElement('div')
       BOX.addEventListener('click', paint)
-      BOX.style.backgroundColor = 'white'
+      BOX.style.backgroundColor = bgcolor
       BOARD.insertAdjacentElement('beforeend', BOX)
    }
 }
 fillBoard(16)
 
 //fill and change color
+function changeBackground(bg){bgcolor = bg; clearCanvas()}
 function changeColor(newcolor){
    let r = parseInt(Math.random() * 256)
    let g = parseInt(Math.random() * 256)
@@ -36,7 +38,7 @@ function changeSize(size){
 
 function clearCanvas() {
    const box = document.querySelectorAll('div')
-   box.forEach(div => div.style.backgroundColor = 'white')
+   box.forEach(div => div.style.backgroundColor = bgcolor)
 }
 
 
